e4341e265b8cba476bce236d927a2fe99463bc9e
jeevanthsait <jeevanthsait@gmail.com>
2025-08-27 10:03:57 +0530

    last-changes-26

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,6 +1,9 @@
+import { Router } from "express";
 import { register, login, getProfile } from "../controllers/authController";
 import { authenticateJWT } from "../middleware/authMiddleware";
-import router from "./userRoutes";
+
+
+const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
